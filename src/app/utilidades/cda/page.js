7e595d966a640af4515b24bbe07855d89ed3f0db
Sebastian Nuñez <imz.sebastian19@gmail.com>
2025-09-10 14:33:03 -0500

    Initial commit

---
+++ src/app/utilidades/cda/page.js
@@ -0,0 +1,8 @@
+export default function Calculadora() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">Calculadora</h1>
+      <p>Aquí irá tu calculadora 🔢</p>
+    </div>
+  )
+}
---
+++ src/app/utilidades/gf/page.js
@@ -0,0 +1,8 @@
+export default function Notas() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">Notas</h1>
+      <p>Aquí podrás guardar tus notas 📝</p>
+    </div>
+  )
+}
